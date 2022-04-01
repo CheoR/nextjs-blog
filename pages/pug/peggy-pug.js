b3d053b9ee0257c0979678b66eda98202fb8ed87
CheoR <cheo.roman+github@gmail.com>
2022-03-31 22:16:04 -0500

    layout: restructure components as children of Layout component example.

--- pages/pug/peggy-pug.js
+++ pages/pug/peggy-pug.js
@@ -1,16 +1,23 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
+import Layout from '../../components/layout'
 
 const PeggyPug = () => {
   return (
-    <div>
-      <h1>Peggy Pug is the best pug</h1>
-       <h2>
-        <Link href="/">
-          <a>Back to "home"</a>
-        </Link>
-      </h2>
-    </div>
+    <Layout>
+      <Head>
+        <title>Pug STuff</title>
+      </Head>
+      <div>
+        <h1>Peggy Pug is the best pug</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to "home"</a>
+          </Link>
+        </h2>
+      </div>
+    </Layout>
   )
 }
 
