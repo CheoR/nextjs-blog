222df2c2d2cbb7df36b4501aa2c971ed023ac3fc
CheoR <cheo.roman+github@gmail.com>
2022-04-02 21:57:21 -0500

    build: dynimically build pages via dynamic routes example.

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  /**
+   * @path : array of known paths returned by getAllPostIds()
+   * which include the params defined by pages/posts/[id].js
+   */
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
