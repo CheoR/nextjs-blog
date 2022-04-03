ef505f7c4a44430f9bbd39b6336a55b2ee4e0f0c
CheoR <cheo.roman+github@gmail.com>
2022-04-03 10:17:51 -0500

    layout: polish dynamically rendered pages with updating date objects and adding more css.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,17 +1,30 @@
+import Head from 'next/head'
+
+import Date from '../../components/date'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+   <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+          <br />
+          Regular Date: {postData.date}
+          <br />
+          With Date component: <Date dateString={postData.date} />
+          <br />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
+
   )
 }
 
